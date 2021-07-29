872e32c46c456f287f28624ef981b44dbd07cbb4
ZAF07 <zaffere07@gmail.com>
2021-07-29 18:34:12 +0800

    for heroku href

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ export function App() {
         !roomID && <button type="button" onClick={handleGetRoomId}>Get room ID</button>
       }
 
-      {/* {
-        roomID && <a href={`https://secret-oasis-97695.herokuapp.com/room/${roomID}`}>Go to room</a>
-      } */}
       {
-        roomID && <a href={`http://localhost:3000/room/${roomID}`}>Go to room</a>
+        roomID && <a href={`https://secret-oasis-97695.herokuapp.com/room/${roomID}`}>Go to room</a>
       }
+      {/* {
+        roomID && <a href={`http://localhost:3000/room/${roomID}`}>Go to room</a>
+      } */}
     </div>
   );
 }
